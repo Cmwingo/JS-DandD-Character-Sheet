60bc60a89fc0056bfb4abc960a30de169f095a08
Wingo <cmwingo@gmail.com>
2017-06-27 17:43:57 -0700

    Wrestle with API CORS policy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'D&D 5th Ed Character Sheet';
+
 }
--- src/app/stats/stats.component.ts
+++ src/app/stats/stats.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Http, Response, Headers } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+declare var $:any;
+
 
 @Component({
   selector: 'app-stats',
@@ -7,9 +13,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StatsComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private router: Router, private http: Http) { }
+  private apiUrl = 'http://dnd5eapi.co/api';
+  classResponse: Response;
   ngOnInit() {
   }
 
+  getBonus(charClass: string) {
+    $('.showClass').text("The class you have chosen is " + charClass + ".");
+
+    // let classes$ = this.http.setRequestHeader('Access-Control-Allow-Origin', '*').get(this.apiUrl).subscribe(data => {
+    //   this.classResponse = data;
+    //   console.log(this.classResponse);
+    // });
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    $.get('http://dnd5eapi.co/api/classes/' + charClass, function(response) {
+      console.log(response);
+    });
+  }
+
+  private getHeaders(){
+    let headers = new Headers();
+    headers.append('Accept', 'application/json');
+    headers.append('Access-Control-Allow-Origin', '*');
+    return headers;
+  }
 }
